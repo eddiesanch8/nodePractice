8ce5139f28a7f92c4cd0cfcbc5dcd440c24a8028
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-08-20 18:51:10 -0400

    added chalk module using import

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,17 @@
-const getNotes = require("./notes.js");
+import chalk from "chalk";
 
-console.log(getNotes());
+console.log(
+  chalk.white.dim.bold.inverse.bgCyan(
+    "Hello Helena, look at all the pretty colors"
+  )
+);
+
+let serverError = "Ok that was my bad bro";
+let userError = "SILLY USER, THATS INCORRECT!!!";
+
+let errorMsg = function (err) {
+  return chalk.bold.red.underline.inverse.bgWhite(`${err}`);
+};
+
+console.log(errorMsg(userError));
+console.log(errorMsg(serverError));
