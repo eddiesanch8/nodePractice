52a8556133db9460dedf3c0fd5bedbdede52be6c
Eduardo <46777616+eddiesanch8@users.noreply.github.com>
2025-08-27 18:56:30 -0400

    finished API calls and refactored code

---
+++ weather-app/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.weatherstack.com/current?access_key=c3bebdc2823598f5e839294ddd795a3a&query=${longitude},${latitude}&units=f`;
+  request({ url: url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("unable to connect to API", error);
+    } else if (body.error) {
+      callback("error, unable to find location", body.error);
+    } else {
+      callback(
+        undefined,
+        `It is currently: ${body.current.temperature} degrees celsius in ${body.location.region}. It feels like ${body.current.feelslike} degrees fahrenheit and the sky is ${body.current.weather_descriptions}`
+      );
+    }
+  });
+};
+
+// c3bebdc2823598f5e839294ddd795a3a
+// Dont share this key
+
+module.exports = forecast;
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,16 +4,16 @@ const geoCode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
     address
   )}.json?access_token=pk.eyJ1IjoiZXNhbmNoZXoxMTIiLCJhIjoiY21ldDFjM2ZjMDh0cDJrb25lcDFtNDRpZCJ9.aaONUWlwbU-JiZL00Za83g&limit=1`;
-  request({ url: url, json: true }, (error, response) => {
+  request({ url: url, json: true }, (error, { body }) => {
     if (error) {
       callback("unable to connect to server", undefined);
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("cannot find location given", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        locations: response.body.features[0].place_name,
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name,
       });
     }
   });
